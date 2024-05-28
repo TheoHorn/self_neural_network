[FIX] derivative and gradian descent fix (needs to be corrected still)

src/Activation_functions.py
@@ -18,23 +18,23 @@ class Activation_functions:
         exp_x = np.exp(x - np.max(x))
         return exp_x / np.sum(exp_x, axis=0)
 
-    def derivate_identity(x):
+    def derivative_identity(x):
         return 1
 
-    def derivate_sigmoid(x):
+    def derivative_sigmoid(x):
         # Use the sigmoid function to compute the derivative
         sigmoid_x = Activation_functions.sigmoid(x)
         return sigmoid_x * (1 - sigmoid_x)
 
-    def derivate_relu(x):
+    def derivative_relu(x):
         return np.where(x > 0, 1, 0)
 
-    def derivate_tanh(x):
+    def derivative_tanh(x):
         # Use the tanh function to compute the derivative
         tanh_x = Activation_functions.tanh(x)
         return 1 - tanh_x ** 2
 
-    def derivate_softmax(x):
+    def derivative_softmax(x):
         # The derivative of the softmax function is more complex and is
         # computed using the following formula:
         # softmax(x) * (1 - softmax(x)) for i = j
@@ -49,3 +49,31 @@ class Activation_functions:
                 else:
                     result[i, j] = -softmax_x[i] * softmax_x[j]
         return result
+
+    def get_value_from_name(name):
+        if name == "identity":
+            return Activation_functions.identity
+        elif name == "sigmoid":
+            return Activation_functions.sigmoid
+        elif name == "relu":
+            return Activation_functions.relu
+        elif name == "tanh":
+            return Activation_functions.tanh
+        elif name == "softmax":
+            return Activation_functions.softmax
+        else:
+            raise ValueError("Activation function not supported")
+        
+    def get_derivative_value_from_name(name):
+        if name == "identity":
+            return Activation_functions.derivative_identity
+        elif name == "sigmoid":
+            return Activation_functions.derivative_sigmoid
+        elif name == "relu":
+            return Activation_functions.derivative_relu
+        elif name == "tanh":
+            return Activation_functions.derivative_tanh
+        elif name == "softmax":
+            return Activation_functions.derivative_softmax
+        else:
+            raise ValueError("Activation function not supported")

src/Error_functions.py
@@ -9,7 +9,7 @@ class Error_functions:
 
     def cross_entropy_error(outputs, targets):
         # Add a small value to avoid log(0)
-        return -np.sum(targets * np.log(outputs + 1e-9))
+        return -np.sum([target * np.log(output + 1e-9) for target, output in zip(targets, outputs)])
 
     def mean_absolute_error(outputs, targets):
         return np.mean(np.abs(outputs - targets))
@@ -19,7 +19,9 @@ class Error_functions:
 
     def derivate_cross_entropy_error(outputs, targets):
         # Add a small value to avoid division by zero
-        return -(targets / (outputs + 1e-9))
+        return [-(target / (output + 1e-9))  for target, output in zip(targets, outputs)]
+
+        
 
     def derivate_mean_absolute_error(outputs, targets):
         # Use np.where to handle the case where outputs - targets == 0
@@ -37,5 +39,17 @@ class Error_functions:
             return Error_functions.mean_absolute_error(outputs, targets)
         elif name == "quadratic_error":
             return Error_functions.quadratic_error(outputs, targets)
+        else:
+            raise ValueError("Error function not supported")
+        
+    def get_derivate_value_from_name(outputs, targets, name):
+        if name == "mean_squared_error":
+            return Error_functions.derivate_mean_squared_error(outputs, targets)
+        elif name == "cross_entropy_error":
+            return Error_functions.derivate_cross_entropy_error(outputs, targets)
+        elif name == "mean_absolute_error":
+            return Error_functions.derivate_mean_absolute_error(outputs, targets)
+        elif name == "quadratic_error":
+            return Error_functions.derivate_quadradic_error(outputs, targets)
         else:
             raise ValueError("Error function not supported")

src/Main.py
@@ -1,43 +1,17 @@
 from Neural_network import Neural_network
 from Layer import Layer
 import numpy as np
+import pandas as pd
 
+inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+targets = np.array([[0], [1], [1], [0]])
 
-nw = Neural_network()
-nw.add_layer(Layer(2, 8,"relu"))
-nw.add_layer(Layer(1, 2,"relu"))
-print(nw)
+nn = Neural_network()
+nn.add_layer(Layer(2, 2))
+nn.add_layer(Layer(1, 2, activation_function="sigmoid"))
 
-inputs = []
-targets = []
-for i in range(10000):
-    input = np.random.rand(8)
-    target = 0
-    target = 0
-    for j in range(8):
-        if j % 2 == 0:
-            target += input[j]
-        else:
-            target += input[j]*0.5
-    inputs.append(input)
-    targets.append([target])
+nn.train(inputs, targets, learning_rate=0.1, error_function="mean_squared_error", max_epochs=10000)
 
-inputs = np.array(inputs)
-targets = np.array(targets)
-
-test_input = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8]
-true_result = 0
-for j in range(8):
-    if j % 2 == 0:
-        true_result += test_input[j]
-    else:
-        true_result += test_input[j] *0.5
-
-
-nw.train(inputs, targets, learning_rate=0.0000001, error_function="mean_squared_error", max_epochs=1)
-one_predicted_result = nw.predict(test_input)
-print("True result with one epoch: ", true_result, "Predicted result: ", one_predicted_result)
-
-nw.train(inputs, targets, learning_rate=0.0000001, error_function="mean_squared_error", max_epochs=1000)
-predicted_result = nw.predict(test_input)
-print("True result: ", true_result, "Predicted result: ", predicted_result)
+new_inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+predictions = nn.predict(new_inputs)
+print(predictions)

src/Neural_network.py
@@ -1,4 +1,3 @@
-import Neuron
 import numpy as np
 import Layer
 from Error_functions import Error_functions
@@ -15,16 +14,16 @@ class Neural_network:
 
     def get_outputs(self):
         return self.layers[-1].get_outputs()
-    
-    def set_first_inputs(self, inputs):
+
+    def set_inputs(self, inputs):
         self.inputs = inputs
 
-    def calculate_outputs(self, inputs):
-        #self.set_first_inputs(inputs)
+    def calculate_outputs(self, inputs=None):
+        if inputs is not None:
+            self.set_inputs(inputs)
         self.layers[0].calculate_outputs(self.inputs)
         for i in range(1, len(self.layers)):
             self.layers[i].calculate_outputs(self.layers[i-1].get_outputs())
-        
 
     def calculate_errors(self, targets, error_function):
         if error_function == "mean_squared_error":
@@ -34,45 +33,70 @@ class Neural_network:
         elif error_function == "mean_absolute_error":
             return Error_functions.mean_absolute_error(self.get_outputs(), targets)
         else:
-            raise ValueError("Error function not supported")
-        
-    def gradient_descent(self, targets, learning_rate, error_function="mean_squared_error"):
-        #calculate outputs
-        self.calculate_outputs(targets)
-
-        #calculate errors
-        error = self.calculate_errors(targets,error_function)
-
-        #retropropagation to obtain deltaW = -alpha * dE/dW
-        for i in range(len(self.layers)-1, 0, -1):
-            for j in range(len(self.layers[i].neurons)):
-                neuron = self.layers[i].neurons[j]
-                for k in range(len(neuron.weights)):
-                    if i == len(self.layers)-1:
-                        neuron.weights[k] -= learning_rate * (neuron.output - targets[j]) * neuron.inputs[k]
-                    else:
-                        neuron.weights[k] -= learning_rate * np.sum([self.layers[i+1].neurons[l].weights[j] * self.layers[i+1].neurons[l].output for l in range(len(self.layers[i+1].neurons))]) * neuron.inputs[k]
-
+            raise ValueError("Error function not supported")       
+    
     def train(self, all_inputs, all_targets, learning_rate=0.00001, error_function="mean_squared_error", max_epochs=1000, min_error=0.0001):
         for epoch in range(max_epochs):
             print("Epoch: ", epoch+1,"/",max_epochs)
             error = 0
             for i in range(len(all_inputs)):
-                self.train_batch(all_inputs[i], all_targets[i], learning_rate, error_function)
+                self.gradient_descent(all_inputs[i], all_targets[i], learning_rate, error_function)
                 error += self.calculate_errors(all_targets[i], error_function)
             error /= len(all_inputs)
             print("Error: ", error)
             if error < min_error:
                 break
     
-    def train_batch(self, inputs, targets, learning_rate, error_function):
-        self.set_first_inputs(inputs)
-        self.gradient_descent(targets, learning_rate, error_function)
-        
-    def predict(self, inputs):
+    def gradient_descent(self, inputs, targets, learning_rate, error_function="mean_squared_error"):
+        # Forward pass
         self.calculate_outputs(inputs)
-        return self.get_outputs()
-    
+
+        # Backward pass
+        output_errors = Error_functions.get_derivate_value_from_name(self.get_outputs(), targets, error_function)
+
+        for i in range(len(self.layers) - 1, 0, -1):
+            layer_errors = np.dot(self.layers[i].neurons[0].weights[1:], output_errors)
+            for j in range(len(self.layers[i].neurons)):
+                neuron = self.layers[i].neurons[j]
+                delta_weights = np.zeros(len(neuron.weights))
+                delta_bias = 0
+
+                for k in range(len(neuron.weights)):
+                    if k == 0:
+                        delta_bias = learning_rate * layer_errors
+                        delta_weights[k] = 0  # No need to update bias in weights array
+                    else:
+                        delta_weights[k] = learning_rate * layer_errors * neuron.inputs[k - 1] * neuron.activation_function_derivative(neuron.get_output())
+
+                neuron.weights -= delta_weights
+                neuron.bias -= delta_bias
+
+            output_errors = layer_errors * neuron.activation_function_derivative(neuron.get_output())
+
+        # Update output layer biases and weights
+        for j in range(len(self.layers[-1].neurons)):
+            neuron = self.layers[-1].neurons[j]
+            delta_weights = np.zeros(len(neuron.weights))
+            delta_bias = 0
+
+            for k in range(len(neuron.weights)):
+                if k == 0:
+                    delta_bias = learning_rate * output_errors
+                    delta_weights[k] = 0  # No need to update bias in weights array
+                else:
+                    delta_weights[k] = learning_rate * output_errors * neuron.inputs[k - 1] * neuron.activation_function_derivative(neuron.get_output())
+
+            neuron.weights -= delta_weights
+            neuron.bias -= delta_bias
+
+
+    def predict(self, all_inputs):
+        outputs = []
+        for i in range(len(all_inputs)):
+            self.calculate_outputs(all_inputs[i])
+            outputs.append(self.get_outputs())
+        return outputs
+
     def save(self, filename):
         with open(filename, "w") as file:
             for layer in self.layers:
@@ -88,7 +112,7 @@ class Neural_network:
             lines = file.readlines()
             i = 0
             while i < len(lines):
-                layer = Layer.Layer(len(lines[i].split()), len(lines[i+1].split()), lines[i+2].strip(), float(lines[i].strip()))
+                layer = Layer.Layer(len(lines[i].split()), len(lines[i+1].split()), lines[i+2].strip().replace('\n', ''))
                 for j in range(len(lines[i+1].split())):
                     layer.neurons[j].set_weights([float(x) for x in lines[i+1+j].split()])
                 self.add_layer(layer)